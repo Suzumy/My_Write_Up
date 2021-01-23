18ab248e29b903e97eedc11fd297e3efdb15686c
suzumy <ryouga.0085@gmail.com>
2021-01-23 14:45:59 +0900

    writeupの新規作成機能追加

--- app/controllers/writeups_controller.rb
+++ app/controllers/writeups_controller.rb
@@ -1,4 +1,20 @@
 class WriteupsController < ApplicationController
   def index
   end
+
+  def new
+    @writeup = Writeup.new
+  end
+
+  def create
+    writeup = Writeup.new(writeup_params)
+    writeup.save!
+    redirect_to writeups_path, notice: "登録完了"
+  end
+
+  private
+
+  def writeup_params
+    params.require(:writeup).permit(:tournament_name, :startdate, :problem_name, :problem_type, :description)
+  end
 end
