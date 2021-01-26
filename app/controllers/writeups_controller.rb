948be2ccdacf6c568b5371d7e14e267a30bd991f
suzumy <ryouga.0085@gmail.com>
2021-01-27 02:45:29 +0900

    CURD機能完成

--- app/controllers/writeups_controller.rb
+++ app/controllers/writeups_controller.rb
@@ -1,5 +1,6 @@
 class WriteupsController < ApplicationController
   def index
+    @writeups = Writeup.all
   end
 
   def new
@@ -10,6 +11,26 @@ class WriteupsController < ApplicationController
     writeup = Writeup.new(writeup_params)
     writeup.save!
     redirect_to writeups_path, notice: "登録完了"
+  end
+
+  def show
+    @writeup = Writeup.find(params[:id])
+  end
+
+  def edit
+    @writeup = Writeup.find(params[:id])
+  end
+
+  def update
+    writeup = Writeup.find(params[:id])
+    writeup.update!(writeup_params)
+    redirect_to writeups_url, notice: "writeup「#{writeup.tournament_name}を更新しました」"
+  end
+
+  def destroy
+    writeup = Writeup.find(params[:id])
+    writeup.destroy
+    redirect_to writeups_url, notice: "記事「#{writeup.tournament_name}を削除しました」"
   end
 
   private
